5331f1294aea77b6c87757c35ffe582444e71d0b
Torishniia <valeri.torishnya@gmail.com>
2022-11-26 10:24:54 +0200

    Adding items to the cart

---
+++ src/components/Order.js
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Component } from 'react'
+
+export class Order extends Component {
+  render() {
+    return (
+      <div className='item'>
+            <img src={"./img/" + this.props.item.img}/>
+            <h2>{this.props.item.title}</h2>
+            <b>{this.props.item.price} грн</b>
+      </div>
+    )
+  }
+}
+
+export default Order
