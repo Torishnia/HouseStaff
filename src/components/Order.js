7d528724a6b197ed3955d67e4e249e215fc9f2d4
Torishniia <valeri.torishnya@gmail.com>
2022-11-27 09:24:10 +0200

    Removing items from the shopping cart

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { FaTrash } from 'react-icons/fa';
 
 export class Order extends Component {
   render() {
@@ -8,6 +9,7 @@ export class Order extends Component {
             <img src={"./img/" + this.props.item.img}/>
             <h2>{this.props.item.title}</h2>
             <b>{this.props.item.price} грн</b>
+            <FaTrash className='delete-icon' onClick={() => this.props.onDelete(this.props.item.id)}/>
       </div>
     )
   }
